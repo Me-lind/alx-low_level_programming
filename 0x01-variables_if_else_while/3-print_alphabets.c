48a37d20e6a1625112bf9a71ac617d8adeb1985b
Super User <melindachebet21@gmail.com>
2022-12-21 23:35:45 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return :Always 0
+ */
+
+int main(void)
+{
+  int n;
+  int m;
+
+  for (n = 97; nn <= 122; n++)
+    {
+      putchar(n);
+    }
+  for (m = 65; m<= 90; m++)
+    {
+      putchar(m);
+    }
+  putchar('\n');
+  return (0);
+}
